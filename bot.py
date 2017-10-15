67c9bb1b0a143c21ac4da85b2948667c531e8068
akashin <kashin.andrej@gmail.com>
2017-10-15 08:37:46 +0300

    Load token from args or env variables.

--- bot.py
+++ bot.py
@@ -22,11 +22,19 @@ class BotHandler(object):
 
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('token')
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--token', type=str, default='')
+    args = parser.parse_args()
 
-    token = os.environ["TELEGRAM_TOKEN"]
+    token = args.token
+    if not token:
+        if not "TELEGRAM_TOKEN" in os.environ:
+            print(
+                "Please, set bot token through --token or TELEGRAM_TOKEN env variable"
+            )
+            return
+        token = os.environ["TELEGRAM_TOKEN"]
+
     bot = BotHandler(token)
     offset = 0
 
